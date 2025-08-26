8224b7dee2c195dab78fc4a3195da95fe8c43e67
MiKPo4eLiK <stassojcuk@gmail.com>
2025-08-26 14:45:11 +0300

    jdfddflk

--- car/serializers.py
+++ car/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
+from .models import Car
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 MIN_HP = 50
 MAX_HP = 1000
 
-
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(
@@ -16,12 +17,13 @@ class CarSerializer(serializers.Serializer):
     )
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(
-        required=False, allow_null=True)
+        required=False, allow_null=True, allow_blank=True
+    )
 
-    def create(self, validated_data) -> dict:
-        return validated_data
+    def create(self, validated_data) -> Car:
+        return Car.objects.create(**validated_data)
 
-    def update(self, instance, validated_data) -> dict:
+    def update(self, instance: Car, validated_data) -> Car:
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         return instance
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import json
 from car.models import Car
 from car.serializers import CarSerializer
 
-
-def serialize_car_object(car_object: Car) -> str:
-    serializer = CarSerializer(car_object)
-    return json.dumps(serializer.data, indent=4)
-
+def serialize_car_object(car: Car) -> bytes:
+    """Серіалізує об'єкт Car у JSON у байтах без пробілів"""
+    from car.serializers import CarSerializer
+    import json
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data, separators=(',', ':')).encode("utf-8")
 
 def deserialize_car_object(json_data: str) -> Car:
     car_data = json.loads(json_data)
     serializer = CarSerializer(data=car_data)
     serializer.is_valid(raise_exception=True)
-    return serializer.save()
+    return Car.objects.create(**serializer.validated_data)

481f13fd69620f104a5bf0c66e51e82328d86ac1
MiKPo4eLiK <stassojcuk@gmail.com>
2025-08-26 13:57:42 +0300

    done

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,25 @@
 from rest_framework import serializers
+from django.core.validators import MinValueValidator, MaxValueValidator
 
+MIN_HP = 50
+MAX_HP = 1000
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[
+            MinValueValidator(MIN_HP),
+            MaxValueValidator(MAX_HP)
+        ]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False, allow_null=True)
+
+    def create(self, validated_data) -> dict:
+        return validated_data
+
+    def update(self, instance, validated_data) -> dict:
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from car.models import Car
+import json
+from car.serializers import CarSerializer
 
+class Car:
+    def __init__(self, manufacturer, model, horse_powers, is_broken, problem_description=None) -> None:
+        self.manufacturer = manufacturer
+        self.model = model
+        self.horse_powers = horse_powers
+        self.is_broken = is_broken
+        self.problem_description = problem_description
 
-def serialize_car_object(car: Car) -> bytes:
-    pass
+def serialize_car_object(car_object) -> str:
+    serializer = CarSerializer(car_object)
+    return json.dumps(serializer.data, indent=4)
 
-
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_data) -> object:
+    car_data = json.loads(json_data)
+    serializer = CarSerializer(data=car_data)
+    if serializer.is_valid():
+        car_instance = serializer.create(serializer.validated_data)
+        return Car(**car_instance)
+    else:
+        print("Validation errors:", serializer.errors)
+        return None

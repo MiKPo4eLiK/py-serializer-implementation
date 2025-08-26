75e0e5d334872a904dcb48f077afe1f29b32bf93
MiKPo4eLiK <stassojcuk@gmail.com>
2025-08-26 14:33:15 +0300

    djbdkvdk

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 import json
+from car.models import Car
 from car.serializers import CarSerializer
 
 
-class Car:
-    def __init__(self, manufacturer, model, horse_powers,
-                 is_broken, problem_description=None) -> None:
-        self.id = id
-        self.manufacturer = manufacturer
-        self.model = model
-        self.horse_powers = horse_powers
-        self.is_broken = is_broken
-        self.problem_description = problem_description
-
-
-def serialize_car_object(car_object) -> str:
+def serialize_car_object(car_object: Car) -> str:
     serializer = CarSerializer(car_object)
     return json.dumps(serializer.data, indent=4)
 
 
-def deserialize_car_object(json_data) -> object:
+def deserialize_car_object(json_data: str) -> Car:
     car_data = json.loads(json_data)
     serializer = CarSerializer(data=car_data)
-    if serializer.is_valid():
-        car_instance = serializer.create(serializer.validated_data)
-        return Car(**car_instance)
-    else:
-        print("Validation errors:", serializer.errors)
-        return None
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
+python manage.py makemigrations
